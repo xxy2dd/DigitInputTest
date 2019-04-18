70ce6bd560c240757eadb9d67cb3ece775d27316
xxy2dd <xxy_cpu@163.com>
2019-04-18 10:05:11 +0800

    init project

---
+++ src/main/java/com/example/demo/DigitInputTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitInputTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitInputTestApplication.class, args);
+	}
+
+}
